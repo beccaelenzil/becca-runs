b20dd2e4d5018a52a8ce89260dffc15913fcb039
Becca <becca@adadevelopersacademy.org>
2021-02-06 17:11:00 -0800

    configuration

--- config.py
+++ config.py
@@ -1,7 +1,1 @@
-FITBIT Application
-
-OAuth 2.0 Client ID: 22C7TR
-Client Secret: d18c3e7e89419b7beca9440de3304517
-Callback URL: https://github.com/beccaelenzil/becca-runs
-OAuth 2.0: Authorization URI: https://www.fitbit.com/oauth2/authorize
-OAuth 2.0: Access/Refresh Token Request URI: https://api.fitbit.com/oauth2/token 
+debug = True
