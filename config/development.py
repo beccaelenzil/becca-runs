abbd8fd77d154622972de98c9d468395b8c59360
Becca <becca@adadevelopersacademy.org>
2021-02-06 17:52:15 -0800

    config

--- config/development.py
+++ config/development.py
@@ -3,4 +3,4 @@ debug = True
 CALLBACK_URL = 'https://github.com/beccaelenzil/becca-runs'
 OAUTH_AUTHORIZATION_URI = 'https://www.fitbit.com/oauth2/authorize'
 OAUTH_REFRESH_TOKEN_URI = 'https://api.fitbit.com/oauth2/token' 
-SQLALCHEMY_DATABASE_URI = "postgresql://user:XXXX@localhost/databasename"
+SQLALCHEMY_DATABASE_URI = "postgresql:///becca_runs"
